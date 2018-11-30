a5e597315b3057b72192e519a1f733c3a86680e6
Austin Lin <alin@chanzuckerberg.com>
2018-11-30 00:21:14 -0800

    Leave profile decoding as an exercise for the user
    Okta has a complex profile system where users can add their own attributes. This means we can't anticipate the names of the fields that might be present in the profile. In order to avoid confusion about that, this change removes parsing of the UserProfile and leaves it as an exercise for the user.

--- okta/user_models.go
+++ okta/user_models.go
@@ -1,6 +1,9 @@
 package okta
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 /*
 {
@@ -86,7 +89,7 @@ type User struct {
 	LastUpdated     time.Time `json:"lastUpdated"`
 	PasswordChanged time.Time `json:"passwordChanged"`
 
-	Profile UserProfile `json:"profile"`
+	RawProfile *json.RawMessage `json:"profile"`
 
 	Credentials UserCredentials `json:"credentials"`
 
@@ -113,37 +116,6 @@ type User struct {
 			Link string `json:"href"`
 		} `json:"changePassword"`
 	} `json:"_links"`
-}
-
-// UserProfile represents the profile object in Okta.
-//
-// https://developer.okta.com/docs/api/resources/users#profile-object
-type UserProfile struct {
-	Login             string `json:"login"`
-	FirstName         string `json:"firstName"`
-	LastName          string `json:"lastName"`
-	NickName          string `json:"nickName"`
-	DisplayName       string `json:"displayName"`
-	Email             string `json:"email"`
-	SecondEmail       string `json:"secondEmail"`
-	ProfileURL        string `json:"profileUrl"`
-	PreferredLanguage string `json:"preferredLanguage"`
-	UserType          string `json:"userType"`
-	Organization      string `json:"organization"`
-	Title             string `json:"title"`
-	Division          string `json:"division"`
-	Department        string `json:"department"`
-	CostCenter        string `json:"costCenter"`
-	EmployeeNumber    string `json:"employeeNumber"`
-	MobilePhone       string `json:"mobilePhone"`
-	PrimaryPhone      string `json:"primaryPhone"`
-	StreetAddress     string `json:"streetAddress"`
-	City              string `json:"city"`
-	State             string `json:"state"`
-	ZipCode           string `json:"zipCode"`
-	CountryCode       string `json:"countryCode"`
-	Manager           string `json:"manager"`
-	ManagerID         string `json:"managerId"`
 }
 
 // UserCredentials represents the credentials object in Okta.
